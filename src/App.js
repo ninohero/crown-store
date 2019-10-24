522452bf4545efc53897ade19316ea9187c8748a
ninohero <ninohero@muzocalo.com>
2019-10-24 14:40:00 -0400

    added collection  preview and collection item components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import { Route, Switch, Link } from 'react-router-dom'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
 
-import Homepage from './pages/homepage/homepage.component'
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.js';
 
 const HatsPage = () => (
  <div>
   <h1>HATS PAGE</h1>
  </div>
-)
+);
 
 function App() {
  return (
@@ -17,9 +18,10 @@ function App() {
    <Switch>
     <Route exact path="/" component={Homepage} />
     <Route path="/hats" component={HatsPage} />
+    <Route path="/shop" component={ShopPage} />
    </Switch>
   </div>
- )
+ );
 }
 
-export default App
+export default App;
