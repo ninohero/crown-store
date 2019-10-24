b493becfe0a107c0fcfa055964ab15a71919e7ad
ninohero <ninohero@muzocalo.com>
2019-10-24 10:24:55 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Homepage from "./homepage.component";
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
